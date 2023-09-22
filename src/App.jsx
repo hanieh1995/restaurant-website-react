2f9691d473457c50353b583442fc3cb17287fb03
Hanieh1995 <hony1374@gmail.com>
2023-09-22 20:40:29 +0330

    ui implement

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import { AboutUs, Chef, FindUs, Footer, Gallery, Header, Intro, Laurels, SpecialMenu } from './container';
+import { Navbar } from './components';
+
+import './App.css';
+
+function App() {
+
+  return (
+    <div>
+    <Navbar />
+    <Header />
+    <AboutUs />
+    <SpecialMenu />
+    <Chef />
+    <Intro />
+    <Laurels />
+    <Gallery />
+    <FindUs />
+    <Footer />
+  </div>
+  )
+}
+
+export default App
---
+++ src/components/SubHeading/SubHeading.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import images from '../../constants/images';
+
+const SubHeading = ({title}) => (
+  <div style={{margin:"1rem"}}>
+    <p className='p__cormorant'>{title}</p>
+    <img src={images.spoon} alt="spoon" className='spoon__img' />
+  </div>
+);
+
+export default SubHeading;
